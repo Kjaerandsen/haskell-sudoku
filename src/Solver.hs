5aacc0a3ed9e09b25024369a0323fedafd378a9f
jonakj <jonakj@stud.ntnu.no>
2022-04-27 22:17:44 +0200

    Added outline for some of the solver functions, as well as tests for them.

--- src/Solver.hs
+++ src/Solver.hs
@@ -1,6 +1,39 @@
 module Solver
-    ( someFunc
+    ( someFunc,
+      solve,
+      validateBoard,
+      validateBoardState
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- >>> buildLineArr [1,2,3]
+-- [[1,2,3],[1,2,3]]
+--
+
+buildLineArr :: [Int] -> [[Int]]
+buildLineArr line = do
+    -- Read each individual integer present in the array.
+
+    -- Fill each not-filled array slot with theese possible integers
+    -- and the old filled slots with "0"
+    [line,line]
+
+solve :: [Char] -> [Char]
+solve x = x
+
+-- >>> validateBoard ["x"]
+-- (Error while loading modules for evaluation)
+-- <BLANKLINE>
+-- Failed, no modules loaded.
+-- <no location info>: error:
+--     module `main:Main' is defined in multiple files: n:\Skole\AdvancedProgramming\haskell-sudoku\app\Main.hs
+--                                                      n:\Skole\AdvancedProgramming\haskell-sudoku\test\Spec.hs
+--
+
+validateBoard :: [Char] -> Bool
+validateBoard x = False
+
+validateBoardState :: [Char] -> Bool
+validateBoardState x = False
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,55 @@
+-- Testing dependencies
+import Test.DocTest(doctest) 
+import Test.QuickCheck(quickCheck) 
+import Test.Hspec(Spec, hspec, describe, shouldBe, it) 
+import Test.Hspec.QuickCheck(prop) 
+
+-- Functions to test
+import Lib
+import Solver
+import GameLogic
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    putStrLn "Doctests:"
+    doctest ["-isrc", "app/Main.hs"]
+    putStrLn "\nHspec tests:"
+    hspec $ do
+        testSolve
+
+testSolve :: Spec
+testSolve =
+    describe "\nTests for the sudoku board auto solver" $ do
+        it "Basic puzzle one" $ do
+            solve "___76_4__2_7___5181__52_3__68_1_9_4_54_8_2_69_7_3_6_25__6_15__7435___2_1__9_83___" `shouldBe` "358761492267934518194528376682159743543872169971346825826415937435697281719283654"
+        it "Basic puzzle two" $ do
+            solve "_5__19__4_3842_____97863___9__5_73_63_4___5_15_26_1__8___17469_____9815_7__35__8_" `shouldBe` "256719834138425769497863215981547326364982571572631948825174693643298157719356482"
+        it "Invalid input, empty output" $ do
+            solve "" `shouldBe` ""
+        it "invalid length, empty output" $ do
+            solve "_______" `shouldBe` ""
+        it "invalid character, empty output" $ do
+            solve "____x___" `shouldBe` ""
+
+testValidateBoard :: Spec
+testValidateBoard = do    
+    describe "\nTests for the sudoku board auto solver" $ do
+        it "valid board" $ do
+            validateBoard "___76_4__2_7___5181__52_3__68_1_9_4_54_8_2_69_7_3_6_25__6_15__7435___2_1__9_83___" `shouldBe` True
+        it "invalid length" $ do
+            validateBoard "" `shouldBe` False
+        it "invalid character" $ do
+            validateBoard "____x___" `shouldBe` False
+
+
+testValidateBoardState :: Spec
+testValidateBoardState = do
+    describe "\nTests for the sudoku board auto solver" $ do
+        it "Basic puzzle one, valid" $ do
+            validateBoardState "___76_4__2_7___5181__52_3__68_1_9_4_54_8_2_69_7_3_6_25__6_15__7435___2_1__9_83___" `shouldBe` True
+        it "conflict vertical" $ do
+            validateBoardState "1________1_______________________________________________________________________" `shouldBe` False
+        it "conflict horizontal" $ do
+            validateBoardState "11_______________________________________________________________________________" `shouldBe` False
+        it "conflict grid" $ do
+            validateBoardState "1____________1___________________________________________________________________" `shouldBe` False
