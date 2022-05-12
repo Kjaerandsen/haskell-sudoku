8a88932b676fe77eca7c5936630b325f78510d15
jonakj <jonakj@stud.ntnu.no>
2022-05-12 23:55:17 +0200

    Now allows a user to input their own puzzle to play

--- app/Main.hs
+++ app/Main.hs
@@ -22,6 +22,21 @@ main = do
             "-hard" -> do
                 printBoard "___76_4__2_7___5181__52_3__68_1_9_4_54_8_2_69_7_3_6_25__6_15__7435___2_1__9_83___"
                 gameLoopHard "___76_4__2_7___5181__52_3__68_1_9_4_54_8_2_69_7_3_6_25__6_15__7435___2_1__9_83___" "___76_4__2_7___5181__52_3__68_1_9_4_54_8_2_69_7_3_6_25__6_15__7435___2_1__9_83___" "358761492267934518194528376682159743543872169971346825826415937435697281719283654"
+            "-board" -> do
+                if length args < 2 then do
+                    putStrLn "-board requires a board as the second argument."
+                    putStrLn "The board should be a string of each horizontal line consequtively"
+                    putStrLn "The length is 81. Use -help for help."
+                else
+                    startGameUNHelper (args!!1)
+            "-solve" -> do
+                if length args < 2 then do
+                    --solveHelper (args!!1)
+                    putStrLn "Placeholder."
+                else do
+                    putStrLn "-solve requires a board as the second argument."
+                    putStrLn "The board should be a string of each horizontal line consequtively"
+                    putStrLn "The length is 81. Use -help for help."
             otherwise -> putStrLn "No valid parameter provided, try running the programs with -help for help.\nExciting the program."
     else do
         putStrLn "No parameters provided, try running the programs with -help for help.\nExciting the program."
